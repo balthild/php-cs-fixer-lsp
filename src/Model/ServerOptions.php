4abcf58f47991f41fc0fed2317161c3c1406c578
Balthild <ibalthild@gmail.com>
2026-02-01 05:18:26 -0600

    A flatter growth curve for number of workers

--- src/Model/ServerOptions.php
+++ src/Model/ServerOptions.php
@@ -36,7 +36,7 @@ final class ServerOptions
         if ($this->workers === 0) {
             $counter = new CpuCoreCounter(FinderRegistry::getDefaultLogicalFinders());
             $cores = $counter->getCountWithFallback(1);
-            $this->workers = \max(1, (int) \log($cores, 2));
+            $this->workers = (int) \log($cores + 1) + 1;
         }
     }
 }
