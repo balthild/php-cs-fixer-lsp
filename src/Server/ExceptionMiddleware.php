75175fe20d577986b60c6b14dabe0c2df46aa7e5
Balthild <ibalthild@gmail.com>
2026-02-02 07:40:02 -0600

    Cleanup

--- src/Server/ExceptionMiddleware.php
+++ src/Server/ExceptionMiddleware.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Balthild\PhpCsFixerLsp\Server;
 
 use Amp\Promise;
--- src/Worker/IpcMainLoop.php
+++ src/Worker/IpcMainLoop.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Balthild\PhpCsFixerLsp\Worker;
 
-use Amp\Failure;
 use Amp\Loop;
 use Amp\Parallel\Sync\ChannelledSocket;
 use Amp\Promise;
